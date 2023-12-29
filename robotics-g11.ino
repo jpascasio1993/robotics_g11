52cad94001d807fa68d7d9fd8ca1777958cd3fc9
Jomar Pascasio <jpascasio1993@gmail.com>
2023-12-29 10:42:41 +0800

    initial commit

---
+++ robotics-g11.ino
@@ -0,0 +1,17 @@
+const int motorPinEnA=6
+const int motorPinIn1=7;
+const int motorPinIn2=8;
+
+const int motorPinEnB=11;
+const int motorPinIn3=12;
+const int motorPinIn4=13;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
