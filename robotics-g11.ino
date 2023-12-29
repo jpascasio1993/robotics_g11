0f43c6c055038d4d436572e4de9b89ad5322fa40
Jomar Pascasio <jpascasio1993@gmail.com>
2023-12-29 10:53:34 +0800

    added comments

--- robotics-g11.ino
+++ robotics-g11.ino
@@ -1,10 +1,10 @@
-const int motorPinEnA=6
-const int motorPinIn1=7;
-const int motorPinIn2=8;
+const int motorPinEnA=6 // connect motor driver EnA pin to arduino uno pin 6 (~ PWM)
+const int motorPinIn1=7; // connect motor driver in1 pin to arduino uno pin 7
+const int motorPinIn2=8; // connect motor driver in2 pin to arduino uno pin 8
 
-const int motorPinEnB=11;
-const int motorPinIn3=12;
-const int motorPinIn4=13;
+const int motorPinEnB=11; // connect motor driver EnA pin to arduino uno pin 11 (~ PWM)
+const int motorPinIn3=12; // connect motor driver in3 pin to arduino uno pin 12
+const int motorPinIn4=13; // connect motor driver in4 pin to arduino uno pin 13
 
 void setup() {
   // put your setup code here, to run once:
